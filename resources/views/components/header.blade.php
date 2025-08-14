d43790133587ca4310a3967492fa17b204b0a410
Atarvano <vanheldyirvan@gmail.com>
2025-08-14 22:08:29 +0700

    Add siswa controller for student management functionality

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg bg-body-white p-3 shadow-sm">
     <div class="container-lg">
-        <a class="navbar-brand fw-bold" href="#">BijakOsis</a>
+        <a class="navbar-brand fw-bold" href="/">BijakOsis</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
             aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -11,7 +11,8 @@
                     <a class="btn btn-outline-primary me-2" aria-current="page" href="#">Masuk</a>
                 </li>
                 <li class="nav-item mb-2 mb-lg-0">
-                    <a class="px-3   btn btn-dark text-white btn-outline-info" aria-current="page" href="#">Daftar</a>
+                    <a class="px-3   btn btn-dark text-white btn-outline-info" aria-current="page"
+                        href="/daftar">Daftar</a>
                 </li>
             </ul>
         </div>
